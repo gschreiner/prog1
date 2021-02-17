b77074dd8d895adb35d718a4a7e9b6ea951d303b
gschreiner <geomarschreiner@gmail.com>
2021-02-17 20:39:38 -0300

    corrigi a identação

--- primeiro.c
+++ primeiro.c
@@ -4,7 +4,7 @@ int main(){
 
 	printf("oi, esse é nosso primeiro programa no git.\n");
 
-		printf("Já queremos fazer mais um commit.\n");
+	printf("Já queremos fazer mais um commit.\n");
 
 	return 0;
 }
