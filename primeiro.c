dd898eac64becc933245a9d6ed3bcb96a2d4add6
Geomar Schreiner <geomarschreiner@gmail.com>
2021-02-17 20:50:16 -0300

    Update primeiro.c

--- primeiro.c
+++ primeiro.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int main(){
-
-	printf("oi, esse é nosso primeiro programa no git.\n");
-
-	printf("Já queremos fazer mais um commit.\n");
 	
-	printf("Vai dar caca.");
+	printf("Salsi fufu!");
 
 	return 0;
 }
